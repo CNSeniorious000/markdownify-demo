eb108691630f0839f88c23f42214b2412a49ef06
Muspi Merol <me@promplate.dev>
2025-03-19 00:10:24 +0800

    chore: update pyodide cdn url

--- src/lib/worker/py.ts
+++ src/lib/worker/py.ts
@@ -3,7 +3,7 @@ import { cacheSingleton } from "$lib/utils/cache";
 
 export const getPy = cacheSingleton(async () => {
   const { loadPyodide, version } = await import("pyodide");
-  const py = await loadPyodide({ indexURL: `https://cdn.py3.online/pyodide/v${version}`, packages: ["micropip"] });
+  const py = await loadPyodide({ indexURL: `https://cdn.promplate.dev/pyodide/v${version}`, packages: ["micropip"] });
   await py.pyimport("micropip.install")("markdownify");
   py.runPython(script);
   return py;
